66feb12b1e2ba3c9b17b22b4fac73b86056c5b0c
JesusPRios <juguitocifrut@gmail.com>
2025-05-28 18:56:37 -0500

    Remove unused authentication API and related components

--- src/prismaCliente.js
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-// Create a new Prisma Client instance to interact with the database
-// This instance can be used to perform database operations throughout the application
-const prisma = new PrismaClient();
-
-export default prisma;
