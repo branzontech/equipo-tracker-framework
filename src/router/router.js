b4b5431c13e3bd90ccde1b63521c3a9163b41810
JesusPRios <juguitocifrut@gmail.com>
2025-05-29 10:50:41 -0500

    Add sedesRoutes to the API router

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,9 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import sedesRoutes from "./routes/sedes.routes.js";
 const router = express.Router();
 
 router.use("/api", authRoutes);
+router.use("/api", sedesRoutes);
 
 export default router;
