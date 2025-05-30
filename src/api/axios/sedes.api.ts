f35e2dac7cb9b183b7b6d7ffe00f1462469767f6
JesusPRios <juguitocifrut@gmail.com>
2025-05-30 15:24:11 -0500

    Add createSede function to handle new sede creation

--- src/api/axios/sedes.api.ts
+++ src/api/axios/sedes.api.ts
@@ -1,6 +1,12 @@
+import { Sede } from "@/pages/configuracion/maestros/interfaces/sedes";
 import api from "./axiosConfig";
 
 export const getSedes = async () => {
     const response = await api.get("/sedes/get");
     return response.data.sedes;
+};
+
+export const createSede = async (sede: Sede) => {
+    const response = await api.post("/sedes/create", sede);
+    return response.data;
 };
