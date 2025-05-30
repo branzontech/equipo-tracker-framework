f33ecae105b8f74afa7752e54dfb76d17a4adc77
JesusPRios <juguitocifrut@gmail.com>
2025-05-30 15:24:16 -0500

    Enhance SedesModel to include user details in findAll and add create function for new sede creation

--- src/db/models/sedes.model.js
+++ src/db/models/sedes.model.js
@@ -2,7 +2,26 @@ import { prisma } from "../../../prisma/prismaCliente.js";
 
 export const SedesModel = {
   findAll: async () => {
-    const sedes = await prisma.sedes.findMany();
+    const sedes = await prisma.sedes.findMany({
+      select: {
+        id_sede: true,
+        descripcion: true,
+        estado: true,
+        usuarios: {
+          select: {
+            id_usuario: true,
+            nombre: true,
+          },
+        },
+      },
+    });
     return sedes;
+  },
+
+  create: async (sede) => {
+    const sedeCreated = await prisma.sedes.create({
+      data: sede,
+    });
+    return sedeCreated;
   },
 };
