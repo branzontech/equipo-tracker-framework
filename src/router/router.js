028c78962a28eaadf5b525120217ad4fa911f2b3
JesusPRios <juguitocifrut@gmail.com>
2025-06-02 13:25:30 -0500

    Add user routes to the API router

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
 import sedesRoutes from "./routes/sedes.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 const routerApi = (app) => {
   const router = express.Router();
@@ -8,6 +9,8 @@ const routerApi = (app) => {
 
   router.use("/auth", authRoutes);
   router.use("/sedes", sedesRoutes);
+  router.use("/users", userRoutes);
+
 };
 
 export default routerApi;
