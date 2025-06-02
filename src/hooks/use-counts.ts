d50515cdf782e50b279751bedc4e0d674e9a1518
JesusPRios <juguitocifrut@gmail.com>
2025-06-02 13:24:40 -0500

    Add userCount to useCounts hook for improved data retrieval

--- src/hooks/use-counts.ts
+++ src/hooks/use-counts.ts
@@ -1,11 +1,12 @@
 import { useSedes } from "@/pages/configuracion/maestros/hooks/use-sedes";
+import { useUser } from "@/pages/usuarios/hooks/use-user";
 
 export const useCounts = () => {
-  const { count } = useSedes();
-  const sedesCount = count;
+  const { count: sedesCount } = useSedes();
+  const { count: userCount } = useUser();
 
   // Aquí puedes agregar más hooks para obtener otros counts si es necesario
   // Por ejemplo, si tienes otro hook para obtener un count de "usuarios":
   // const { count: userCount } = useUsers();
-  return { sedesCount };
+  return { sedesCount, userCount };
 };
