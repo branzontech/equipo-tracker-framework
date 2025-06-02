129789a34f920c6bac266406f91552647efb51bc
JesusPRios <juguitocifrut@gmail.com>
2025-06-02 13:24:36 -0500

    Add UserModel with findAll method to retrieve user data from the database

---
+++ src/db/models/user.model.js
@@ -0,0 +1,23 @@
+import { prisma } from "../../../prisma/prismaCliente.js";
+
+export const UserModel = {
+  findAll: async () => {
+    const users = await prisma.usuarios.findMany({
+      select: {
+        id_usuario: true,
+        nombre: true,
+        email: true,
+        rol: true,
+        activo: true,
+        sede_id: true,
+        sedes: {
+          select: {
+            id_sede: true,
+            descripcion: true,
+          },
+        },
+      },
+    });
+    return users;
+  },
+};
