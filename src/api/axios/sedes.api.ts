f1cc6c83455c2e9f5d0f2042496842c1b68bd48d
JesusPRios <juguitocifrut@gmail.com>
2025-06-03 16:31:24 -0500

    Add deleteSede function to manage Sede deletion in the API

--- src/api/axios/sedes.api.ts
+++ src/api/axios/sedes.api.ts
@@ -10,3 +10,8 @@ export const createSede = async (sede: Sede) => {
     const response = await api.post("/sedes/create", sede);
     return response.data;
 };
+
+export const deleteSede = async (id: number) => {
+    const response = await api.delete(`/sedes/delete/${id}`);
+    return response.data;
+};
