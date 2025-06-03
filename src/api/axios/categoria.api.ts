91919cd9dbf33cfe491c1767a65f26904149c76a
JesusPRios <juguitocifrut@gmail.com>
2025-06-03 16:31:17 -0500

    Add categoria.api.ts for managing category API endpoints

---
+++ src/api/axios/categoria.api.ts
@@ -0,0 +1,17 @@
+import { Categoria } from "@/pages/configuracion/maestros/interfaces/categorias";
+import api from "./axiosConfig";
+
+export const getAllCategorias = async () => {
+  const response = await api.get("/categorias/get");
+  return response.data;
+};
+
+export const createCategoria = async (categoria: Categoria) => {
+  const response = await api.post("/categorias/create", categoria);
+  return response.data;
+};
+
+export const deleteCategoria = async (id: number) => {
+  const response = await api.delete(`categorias/delete/${id}`);
+  return response.data;
+};
