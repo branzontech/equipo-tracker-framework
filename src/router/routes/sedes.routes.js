fcd0ba7769aa0521c26760a4b65204eb46aa9b61
JesusPRios <juguitocifrut@gmail.com>
2025-06-03 16:33:09 -0500

    Add delete route for Sedes to enable removal of sede entries

--- src/router/routes/sedes.routes.js
+++ src/router/routes/sedes.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   getSedes,
   createSede,
+  deleteSede,
 } from "../../pages/configuracion/maestros/controller/sedes.controller.js";
 const router = Router();
 
 router
   .get("/get", getSedes)
-  .post("/create", createSede);
+  .post("/create", createSede)
+  .delete("/delete/:id", deleteSede);
 
 export default router;
