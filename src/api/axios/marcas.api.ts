7247f33e56a872b8015391cede88e312c4b3b55c
JesusPRios <juguitocifrut@gmail.com>
2025-06-03 16:31:21 -0500

    Add marcas.api.ts for managing Marca API endpoints

---
+++ src/api/axios/marcas.api.ts
@@ -0,0 +1,17 @@
+import { Marca } from "@/pages/configuracion/maestros/interfaces/marcas";
+import api from "./axiosConfig";
+
+export const getAllMarcas = async () => {
+  const response = await api.get("/marcas/get");
+  return response.data;
+};
+
+export const createMarca = async (marca: Marca) => {
+  const response = await api.post("/marcas/create", marca);
+  return response.data;
+};
+
+export const deleteMarca = async (id: number) => {
+  const response = await api.delete(`marcas/delete/${id}`);
+  return response.data;
+};
