6a230d782715dca989aaf778701862c676e51d58
JesusPRios <juguitocifrut@gmail.com>
2025-06-03 16:32:49 -0500

    Add Categoria component for managing category entries

---
+++ src/pages/configuracion/maestros/views/Categoria.tsx
@@ -0,0 +1,116 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { PlusCircle, Globe, Phone, PencilIcon, XCircle } from "lucide-react";
+import { useCategoria } from "../hooks/use-categoria";
+
+const Categoria = () => {
+  const {
+    categoria,
+    newCategoria,
+    setNewCategoria,
+    addCategoria,
+    handleDelete,
+  } = useCategoria();
+
+  return (
+    <div className="container mx-auto p-6 space-y-6">
+      <Card className="mb-6">
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold">
+            Registro de Categorias
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form className="grid gap-4 md:grid-cols-4">
+            <div className="space-y-2">
+              <Label htmlFor="nombre">Nombre</Label>
+              <Input
+                id="nombre"
+                value={newCategoria.nombre}
+                onChange={(e) =>
+                  setNewCategoria({ ...newCategoria, nombre: e.target.value })
+                }
+                placeholder="Nombre de la marca"
+                required
+                autoComplete="off"
+              />
+            </div>
+            <div className="flex items-end">
+              <Button
+                type="submit"
+                className="w-full"
+                onClick={(e: React.FormEvent) => {
+                  e.preventDefault();
+                  addCategoria(newCategoria);
+                }}
+              >
+                <PlusCircle className="mr-2 h-4 w-4" />
+                Agregar Categorias
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Lista de Marcas</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Nombre</TableHead>
+                {/* <TableHead>Teléfono</TableHead>
+                <TableHead>Sitio Web</TableHead> */}
+                <TableHead className="text-right">Acciones</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {categoria.map((categoria) => (
+                <TableRow key={categoria.id_categoria}>
+                  <TableCell>{categoria.nombre}</TableCell>
+                  <TableCell className="text-right">
+                    <Button
+                      variant="ghost"
+                      size="icon"
+                      className="text-blue-500 hover:bg-blue-100"
+                      onClick={() => {
+                        console.log("Editar sede:", categoria);
+                      }}
+                    >
+                      <PencilIcon className="h-5 w-5" />
+                    </Button>
+                    <Button
+                      variant="ghost"
+                      size="icon"
+                      className="text-red-500 hover:bg-red-100"
+                      onClick={() => {
+                        handleDelete(categoria.id_categoria);
+                      }}
+                    >
+                      <XCircle className="h-5 w-5" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Categoria;
