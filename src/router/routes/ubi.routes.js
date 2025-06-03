6ddbcbd4423a15c1f29e492be5eaaeb2419afdbb
JesusPRios <juguitocifrut@gmail.com>
2025-06-03 16:33:11 -0500

    Add delete route for Ubicaciones to enable removal of ubicacion entries

--- src/router/routes/ubi.routes.js
+++ src/router/routes/ubi.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getUbi, createUbi } from "../../pages/configuracion/maestros/controller/ubi.controller.js";
+import { getUbi, createUbi, deleteUbi } from "../../pages/configuracion/maestros/controller/ubi.controller.js";
 const router = Router();
 
 router
     .get("/get", getUbi)
-    .post("/create", createUbi);
+    .post("/create", createUbi)
+    .delete("/delete/:id", deleteUbi);
 
 export default router;
