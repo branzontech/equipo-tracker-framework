d2454a0ed47ea399a754a5a27d1b78d27270ba8b
JesusPRios <juguitocifrut@gmail.com>
2025-06-03 12:24:26 -0500

    Add API functions for retrieving and registering locations

---
+++ src/api/axios/ubi.api.ts
@@ -0,0 +1,12 @@
+import { Ubicacion } from "@/pages/configuracion/maestros/interfaces/ubicaciones";
+import api from "./axiosConfig";
+
+export const getUbicaciones = async () => {
+  const response = await api.get("/ubicaciones/get");
+  return response.data.ubicaciones;
+};
+
+export const registerUbicacion = async (ubicacion: Ubicacion) => {
+    const response = await api.post("/ubicaciones/create", ubicacion);
+    return response.data;
+};
