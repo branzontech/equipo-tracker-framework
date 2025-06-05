f2e398a8365e63034aad5fa400d4ccf33bf89047
JesusPRios <juguitocifrut@gmail.com>
2025-06-05 15:40:15 -0500

    Add perifericoService with findAll, create, and delete methods

---
+++ src/pages/configuracion/maestros/services/periferico.service.js
@@ -0,0 +1,13 @@
+import { PerifericoModel } from "../../../../db/models/periferico.models.js";
+
+export const perifericoService = {
+  async findAll() {
+    return await PerifericoModel.findAll();
+  },
+  async create(periferico) {
+    return await PerifericoModel.create(periferico);
+  },
+  async delete(id) {
+    return await PerifericoModel.delete(id);
+  },
+};
