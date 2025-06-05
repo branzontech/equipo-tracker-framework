a63d82dc63e95bb54941f3368a1591d90e23125e
JesusPRios <juguitocifrut@gmail.com>
2025-06-04 22:29:52 -0500

    Add Equipo interface for managing equipment data structure

---
+++ src/pages/productos/interfaces/equipo.ts
@@ -0,0 +1,60 @@
+import { Categoria } from "@/pages/configuracion/maestros/interfaces/categorias";
+import { Marca } from "@/pages/configuracion/maestros/interfaces/marcas";
+import { Sucursal } from "@/pages/configuracion/maestros/interfaces/sucursales";
+
+export interface Equipo {
+  id_equipo: number;
+  nombre_equipo: string;
+  nro_serie: string;
+  modelo: string;
+  marca_id: Marca | number;
+  categoria_id: Categoria | number;
+  tipo_activo: string;
+  fecha_registro: string;
+  sucursal_id: Sucursal | number;
+  garantia_fecha_fin: string;
+  estado_actual: string;
+  observaciones?: string;
+
+  // Información Técnica
+  especificaciones?: {
+    procesador: string;
+    memoria_ram: string;
+    almacenamiento: string;
+    tarjeta_grafica?: string;
+    pantalla?: string;
+    sistema_operativo?: string;
+    bateria?: string;
+    puertos?: string;
+  };
+
+  // Seguridad
+  seguridad?: {
+    nivel_acceso: string;
+    software_seguridad?: string;
+    cifrado_disco?: string;
+    politicas_aplicadas?: string[];
+  };
+
+  // Adquisición
+  adquisicion?: {
+    orden_compra: string;
+    fecha_compra: string;
+    precio_compra: number;
+    forma_pago: string;
+    plazo_pago: string;
+    numero_factura: string;
+    proveedor: string;
+  };
+
+  // Información administrativa
+  informacionAdministrativa?: {
+    codigo_inventario: string;
+    centro_coste: string;
+    autorizado_por: string;
+    fecha_activacion: string;
+    estado_contable: string;
+    valor_depreciado: number;
+    vida_util_restante: string;
+  };
+}
