3be9926a4d7efee1332d0a0f9c4a820bc825c8f2
JesusPRios <juguitocifrut@gmail.com>
2025-06-05 15:40:47 -0500

    Add delete route to equipo API for removing equipos

--- src/router/routes/equipo.routes.js
+++ src/router/routes/equipo.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { create, findAll, findAllById } from "../../pages/productos/controller/equipo.controller.js";
+import { create, findAll, findAllById, delete_ } from "../../pages/productos/controller/equipo.controller.js";
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ router
     .post("/create", create)
     .get("/get", findAll)
     .get("/:nro_serie", findAllById)
+    .delete("/:id", delete_);
 
 export default router;
