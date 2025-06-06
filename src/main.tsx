ef7e25a3a05d6176bae20625be7d76594d829dd7
JesusPRios <juguitocifrut@gmail.com>
2025-06-06 16:53:29 -0500

    Add Toaster component to main.tsx for notifications

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import Cookies from "js-cookie";
+import { Toaster } from "sonner";
 import "./styles/index.css";
 
 // Import the userId from cookies
@@ -24,11 +25,18 @@ root.render(
         // This allows redux-persist to restore the auth state (e.g., the logged-in user)
         <PersistGate loading={null} persistor={persistor}>
           <App />
+          <Toaster richColors position="top-right" />
         </PersistGate>
       ) : (
         // If the user is NOT logged in, skip redux-persist to avoid hydration errors
         // The App will render normally without loading persisted auth state
-        <App />
+        <>
+          // If the user is NOT logged in, skip redux-persist to avoid hydration
+          errors // The App will render normally without loading persisted auth
+          state
+          <App />
+          <Toaster richColors position="top-right" />
+        </>
       )
     }
   </Provider>
