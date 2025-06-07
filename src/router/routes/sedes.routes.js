ba144095628c20dc411bec347417da5ba2dc8d75
JesusPRios <juguitocifrut@gmail.com>
2025-06-07 13:23:33 -0500

    Add getSedeById and updateSede methods to sedes routes

--- src/router/routes/sedes.routes.js
+++ src/router/routes/sedes.routes.js
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import {
   getSedes,
+  getSedeById,
   createSede,
   deleteSede,
+  updateSede,
 } from "../../pages/configuracion/maestros/controller/sedes.controller.js";
 const router = Router();
 
 router
   .get("/get", getSedes)
+  .get("/get/:id", getSedeById)
   .post("/create", createSede)
-  .delete("/delete/:id", deleteSede);
+  .delete("/delete/:id", deleteSede)
+  .put("/update/:id", updateSede)
 
 export default router;
