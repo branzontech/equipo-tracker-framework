a36824d042377a0e9b06bbb8bbfbb6f3e98cbd97
JesusPRios <juguitocifrut@gmail.com>
2025-06-08 13:29:07 -0500

    Add CRUD functions for toner management in the API

---
+++ src/api/axios/toners.api.ts
@@ -0,0 +1,27 @@
+import { Toner } from "@/pages/toners/interfaces/toners";
+import api from "./axiosConfig";
+
+export const getToners = async () => {
+  const response = await api.get("/toners/get");
+  return response.data.toners;
+};
+
+export const getTonerById = async (id: number) => {
+  const response = await api.get(`/toners/get/${id}`);
+  return response.data.toner;
+};
+
+export const createToner = async (toner: Toner) => {
+  const response = await api.post("/toners/create", toner);
+  return response.data;
+};
+
+export const updateToner = async (id: number, toner: Toner) => {
+  const response = await api.put(`/toners/update/${id}`, toner);
+  return response.data;
+};
+
+export const deleteToner = async (id: number) => {
+  const response = await api.delete(`/toners/delete/${id}`);
+  return response.data;
+};
