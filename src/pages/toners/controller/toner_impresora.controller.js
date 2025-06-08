df66e605667beb693207ebe8ed4ac00dcb70d785
JesusPRios <juguitocifrut@gmail.com>
2025-06-08 13:29:49 -0500

    Add getAll method to tonerImpresora controller for retrieving toner data

---
+++ src/pages/toners/controller/toner_impresora.controller.js
@@ -0,0 +1,10 @@
+import { tonerImpresoraService } from "../services/toner_impresora.service.js";
+
+export const getAll = async (req, res) => {
+  try {
+    const tonerImpresora = await tonerImpresoraService.getAll();
+    res.status(200).json({ tonerImpresora });
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
+};
