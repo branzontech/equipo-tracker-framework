2f550a298edb0829dacbaea811d5567c1b46a781
JesusPRios <juguitocifrut@gmail.com>
2025-06-08 13:29:53 -0500

    Implement toner controller methods for CRUD operations

---
+++ src/pages/toners/controller/toners.controller.js
@@ -0,0 +1,51 @@
+import { tonersService } from "../services/toners.service.js";
+
+export const getAll = async (req, res) => {
+  try {
+    const toners = await tonersService.getAll();
+    res.status(200).json({ toners });
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
+};
+
+export const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const toner = await tonersService.getById(id);
+    res.status(200).json({ toner });
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
+};
+
+export const createToner = async (req, res) => {
+  try {
+    const toner = req.body;
+    const tonerCreated = await tonersService.create(toner);
+    res.status(201).json({ success: true, tonerCreated });
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
+};
+
+export const updateToner = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const toner = req.body;
+    const updatedToner = await tonersService.update(id, toner);
+    res.status(200).json({ updatedToner });
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
+};
+
+export const deleteToner = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedToner = await tonersService.delete(id);
+    res.status(200).json({ deletedToner });
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
+};
