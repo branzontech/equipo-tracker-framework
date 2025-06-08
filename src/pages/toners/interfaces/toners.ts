37efa41fb1997c854ff5ede53e71c7c9e2a9b10a
JesusPRios <juguitocifrut@gmail.com>
2025-06-08 13:30:21 -0500

    Refactor Toner interface to replace 'compatible_con' with 'impresoras' property

--- src/pages/toners/interfaces/toners.ts
+++ src/pages/toners/interfaces/toners.ts
@@ -1,10 +1,11 @@
+import { Impresora } from "./impresora";
 
 export interface Toner {
   id_toner: number;
   modelo: string;
   color: string;
   cantidad: number;
-  compatible_con: string;
   stock_actual: number;
   stock_minimo_alerta: number;
+  impresoras: number[];
 }
