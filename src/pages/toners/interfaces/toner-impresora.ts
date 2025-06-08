16a17f5bd2306f6fd30f7e1b307e078097a08f0e
JesusPRios <juguitocifrut@gmail.com>
2025-06-08 13:30:17 -0500

    Add TonerImpresora interface for linking toner and printer data

---
+++ src/pages/toners/interfaces/toner-impresora.ts
@@ -0,0 +1,9 @@
+import { Impresora } from "./impresora";
+import { Toner } from "./toners";
+
+export interface TonerImpresora {
+  toner_id: number;
+  impresora_id: number;
+  toner: Toner;
+  impresora: Impresora;
+}
