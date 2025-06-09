fc38a51315c56558632592c1122ba265614c13d7
JesusPRios <juguitocifrut@gmail.com>
2025-06-09 11:32:37 -0500

    Remove error handling for incorrect user credentials in login method

--- src/pages/auth/services/auth.service.js
+++ src/pages/auth/services/auth.service.js
@@ -4,9 +4,6 @@ import { AuthModel } from "../../../db/models/auth.model.js";
 class AuthService {
   async login(nombre, contraseña) {
     const user = await AuthModel.findByCredentials(nombre, contraseña);
-
-    if (!user) throw new Error("User and password are incorrect");
-
     delete user.contrase_a;
     return user;
   }
