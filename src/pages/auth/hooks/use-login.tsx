13d6172e3f2b4ac0dc9d9553fd62f42d0de7be44
JesusPRios <juguitocifrut@gmail.com>
2025-06-09 11:32:33 -0500

    Refactor useLogin hook to include remember me functionality and improve error handling

--- src/pages/auth/hooks/use-login.tsx
+++ src/pages/auth/hooks/use-login.tsx
@@ -1,30 +1,65 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-// src/hooks/useLogin.ts
 import Cookies from "js-cookie";
 import { loginUser } from "@/api/axios/auth.api";
 import { loginSuccess } from "@/redux/authSlice";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 export const useLogin = () => {
+  const [user, setUser] = useState({
+    nombre: "",
+    contraseña: "",
+  });
+  const [remember, setRemember] = useState(false);
+
+  useEffect(() => {
+    const savedUser = localStorage.getItem("rememberUser");
+    const savedPassword = localStorage.getItem("rememberPassword");
+    if (savedUser && savedPassword) {
+      setUser({ nombre: savedUser, contraseña: savedPassword });
+    }
+  }, []);
+
   const SignIn = async (
     nombre: string,
     contraseña: string,
     dispatch: any,
-    navigate: any
+    navigate: any,
+    remember
   ) => {
     try {
+      if (!nombre) {
+        toast.error("Debe ingresar un nombre de usuario");
+        return;
+      }
+
+      if (!contraseña) {
+        toast.error("Debe ingresar una contraseña");
+        return;
+      }
+
       const { user } = await loginUser(nombre, contraseña);
 
       dispatch(loginSuccess(user));
       Cookies.set("userId", user.id_usuario, { expires: 1 });
       navigate("/dashboard");
 
+      if (remember) {
+        rememberAccount(nombre, contraseña);
+      } else {
+        localStorage.removeItem("rememberUser");
+      }
+
       return { success: true, user };
-    } catch (err: any) {
-      throw new Error(
-        err.response?.data?.error || "Usuario o contraseña incorrectos"
-      );
+    } catch (error) {
+      toast.info(error.message);
     }
   };
 
-  return { SignIn };
+  const rememberAccount = (nombre, contraseña) => {
+    localStorage.setItem("rememberUser", nombre);
+    localStorage.setItem("rememberPassword", contraseña);
+  };
+
+  return { SignIn, user, setUser, rememberAccount, remember, setRemember };
 };
