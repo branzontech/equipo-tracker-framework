d7a7069e1ef704ff46cc317054fae78f275461f3
JesusPRios <juguitocifrut@gmail.com>
2025-06-09 11:52:35 -0500

    Refactor sedes.api.ts for consistent indentation and error handling

--- src/api/axios/sedes.api.ts
+++ src/api/axios/sedes.api.ts
@@ -2,26 +2,30 @@ import { Sede } from "@/pages/configuracion/maestros/interfaces/sedes";
 import api from "./axiosConfig";
 
 export const getSedes = async () => {
-    const response = await api.get("/sedes/get");
-    return response.data.sedes;
+  const response = await api.get("/sedes/get");
+  return response.data.sedes;
 };
 
 export const getSedeById = async (id: number) => {
-    const response = await api.get(`/sedes/get/${id}`);
-    return response.data.sede;
+  const response = await api.get(`/sedes/get/${id}`);
+  return response.data.sede;
 };
 
 export const createSede = async (sede: Sede) => {
-    const response = await api.post("/sedes/create", sede);
-    return response.data;
+  const response = await api.post("/sedes/create", sede);
+  return response.data;
 };
 
 export const deleteSede = async (id: number) => {
+  try {
     const response = await api.delete(`/sedes/delete/${id}`);
     return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message);
+  }
 };
 
 export const updateSede = async (id: number, sede: Sede) => {
-    const response = await api.put(`/sedes/update/${id}`, sede);
-    return response.data;
+  const response = await api.put(`/sedes/update/${id}`, sede);
+  return response.data;
 };
