06af4f49ef2f2ecc8fa648aed0d262c2da9f0d37
JesusPRios <juguitocifrut@gmail.com>
2025-06-09 11:32:13 -0500

    Add error handling to loginUser function in auth.api.ts

--- src/api/axios/auth.api.ts
+++ src/api/axios/auth.api.ts
@@ -1,6 +1,11 @@
 import api from "./axiosConfig";
 
 export const loginUser = async (nombre: string, contraseña: string) => {
-  const response = await api.post("/auth/login", { nombre, contraseña });
-  return response.data; 
+  try {
+    const response = await api.post("/auth/login", { nombre, contraseña });
+    return response.data;
+  } catch (error) {
+    console.error("Error de login:", error);
+    throw new Error(error.response?.data?.message);
+  }
 };
