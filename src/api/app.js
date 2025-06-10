4f9b40e9f2b7e21f352025468f323902ff8c2760
JesusPRios <juguitocifrut@gmail.com>
2025-06-09 23:37:54 -0500

    Refactor CORS configuration and improve JSON request handling in app.js

--- src/api/app.js
+++ src/api/app.js
@@ -1,33 +1,42 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { config } from '../config/config.js';
-import routerApi from '../router/router.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { config } from "../config/config.js";
+import routerApi from "../router/router.js";
 
 dotenv.config();
 const app = express();
 
 // Configuración de CORS
-app.use(cors({
-  origin: (origin, callback) => {
-    const allowedOriginsPattern = /^http:\/\/localhost:(8080|5173|5176|5174|3001)$/;
-    const allowedIpPattern = /^http:\/\/192\.168\.1\.9:(8080|5173|5176|5174|3306|3002)$/;
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const allowedOriginsPattern =
+        /^http:\/\/localhost:(8080|5173|5176|5174|3001)$/;
+      const allowedIpPattern =
+        /^http:\/\/192\.168\.1\.9:(8080|5173|5176|5174|3306|3002)$/;
 
-    if (allowedOriginsPattern.test(origin) || allowedIpPattern.test(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-}));
+      if (
+        allowedOriginsPattern.test(origin) ||
+        allowedIpPattern.test(origin) ||
+        !origin
+      ) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 routerApi(app);
 
 const port = config().port || 3003;
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`🚀 Server listening on port ${port}`);
 });
