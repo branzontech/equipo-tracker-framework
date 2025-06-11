7a14274952cce5a618fef8b0409a1173ef98da23
JesusPRios <juguitocifrut@gmail.com>
2025-06-11 11:29:13 -0500

    Add saveSign route to prestamo router for signature saving functionality

--- src/router/routes/prestamo.routes.js
+++ src/router/routes/prestamo.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createPrestamo, getPrestamos } from "../../pages/productos/controller/prestamo.controller.js";
+import { createPrestamo, getPrestamos, saveSign } from "../../pages/productos/controller/prestamo.controller.js";
 
 const router = express.Router();
 
 router
     .post("/create", createPrestamo)
     .get("/get", getPrestamos)
+    .post("/saveSign", saveSign)
 
 export default router;
