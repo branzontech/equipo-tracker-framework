9dede7527f4e3e8ab1e91a3a66a00a8249213fbf
JesusPRios <juguitocifrut@gmail.com>
2025-06-11 11:28:44 -0500

    Refactor getPrestamos method for improved error handling and consistency; add saveSign method for handling signatures

--- src/pages/productos/controller/prestamo.controller.js
+++ src/pages/productos/controller/prestamo.controller.js
@@ -12,9 +12,25 @@ export const createPrestamo = async (req, res) => {
 
 export const getPrestamos = async (req, res) => {
   try {
-    const prestamos = await PrestamoService.findAll();
+    const prestamos = await PrestamoService.getAll();
     res.status(200).json(prestamos);
   } catch (error) {
-    res.status(500).json({ error: "Error getting all prestamos" });
+    res.status(500).json({ error: error.message });
   }
 };
+
+export const saveSign = async (req, res) => {
+  try {
+    const { firma_entrega, firma_salida, responsable_salida_id, responsable_entrada_id } = req.body;
+    const updatedPrestamo = await PrestamoService.saveSign(
+      firma_entrega,
+      firma_salida,
+      responsable_salida_id,
+      responsable_entrada_id
+    );
+    res.status(200).json({ updatedPrestamo, success: true });
+  } catch (error) {
+    res.status(500).json({ error: "Error saving sign" });
+  }
+};
+
