34ead9eabbf7e364eb0bd23468e64ef0c4678cc8
JesusPRios <juguitocifrut@gmail.com>
2025-06-11 11:28:58 -0500

    Refactor Prestamo interface to remove unused fields and include equipos with associated perifericos

--- src/pages/productos/interfaces/prestamo.ts
+++ src/pages/productos/interfaces/prestamo.ts
@@ -1,14 +1,14 @@
-
 export interface Prestamo {
   id_prestamo: number;
   acta_id: number;
-  equipo_id: number;
   responsable_salida_id: number;
   responsable_entrada_id: number;
-  ubicacion_destino_id: number;
   fecha_salida: Date;
   fecha_retorno: Date;
   estado: string;
-  firma_entrega: string;
-  firma_recibe: string;
+  descripcion: string;
+  equipos: {
+    id_equipo: number;
+    perifericos: number[]; // IDs de los periféricos asociados a este equipo
+  }[];
 }
