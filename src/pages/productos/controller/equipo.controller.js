628a4615f22b064a869f38ec82b75d687a2c7b32
JesusPRios <juguitocifrut@gmail.com>
2025-06-12 18:06:31 -0500

    Refactor error handling in equipo controller methods

--- src/pages/productos/controller/equipo.controller.js
+++ src/pages/productos/controller/equipo.controller.js
@@ -1,58 +1,74 @@
 import { equipoService } from "../services/equipo.service.js";
 
 export const create = async (req, res) => {
-  const {
-    nombre_equipo,
-    nro_serie,
-    modelo,
-    marca_id,
-    categoria_id,
-    sucursal_id,
-    estado_actual,
-    fecha_registro,
-    tipo_activo,
-    garantia_fecha_fin,
-    observaciones,
-    especificaciones,
-    seguridad,
-    adquisicion,
-    administrativa,
-  } = req.body;
+  try {
+    const {
+      nombre_equipo,
+      nro_serie,
+      modelo,
+      marca_id,
+      categoria_id,
+      sucursal_id,
+      estado_actual,
+      fecha_registro,
+      tipo_activo,
+      garantia_fecha_fin,
+      observaciones,
+      especificaciones,
+      seguridad,
+      adquisicion,
+      administrativa,
+    } = req.body;
 
-  const equipo = await equipoService.create({
-    nombre_equipo,
-    nro_serie,
-    modelo,
-    marca_id,
-    categoria_id,
-    sucursal_id,
-    estado_actual,
-    fecha_registro,
-    tipo_activo,
-    garantia_fecha_fin,
-    observaciones,
-    especificaciones,
-    seguridad,
-    adquisicion,
-    administrativa,
-  });
+    const equipo = await equipoService.create({
+      nombre_equipo,
+      nro_serie,
+      modelo,
+      marca_id,
+      categoria_id,
+      sucursal_id,
+      estado_actual,
+      fecha_registro,
+      tipo_activo,
+      garantia_fecha_fin,
+      observaciones,
+      especificaciones,
+      seguridad,
+      adquisicion,
+      administrativa,
+    });
 
-  res.status(201).json({ success: true, data: equipo });
+    res.status(201).json({ success: true, data: equipo });
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
 };
 
 export const findAll = async (req, res) => {
-  const equipos = await equipoService.findAll();
-  res.status(200).json(equipos);
+  try {
+    const equipos = await equipoService.findAll();
+    res.status(200).json(equipos);
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
 };
 
 export const findAllById = async (req, res) => {
-  const { nro_serie } = req.params;
-  const equipo = await equipoService.findById(nro_serie);
-  res.status(200).json(equipo);
+  try {
+    const { nro_serie } = req.params;
+    const equipo = await equipoService.findById(nro_serie);
+    res.status(200).json(equipo);
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
 };
 
 export const delete_ = async (req, res) => {
-  const { id } = req.params;
-  const equipo = await equipoService.delete_(id);
-  res.status(200).json({ success: true, data: equipo });
+  try {
+    const { id } = req.params;
+    const equipo = await equipoService.delete_(id);
+    res.status(200).json({ success: true, data: equipo });
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
 };
