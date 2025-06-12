6a6793759b522319732899cdc9e67f06ec77146b
JesusPRios <juguitocifrut@gmail.com>
2025-06-12 12:28:07 -0500

    Fix layout by adjusting max-width of Salidas component

--- src/pages/productos/views/Salidas.tsx
+++ src/pages/productos/views/Salidas.tsx
@@ -243,7 +243,7 @@ const Salidas = () => {
   const methods = useForm();
 
   return (
-    <div className="p-8 max-w-5xl mx-auto">
+    <div className="p-8 max-w-4xl mx-auto">
       <h1 className="text-2xl font-bold text-[#01242c] mb-6">
         Salida de Equipos en Condición de Préstamo
       </h1>
