af62f238cd4f4716808fd49a41b78c01f3719cae
JesusPRios <juguitocifrut@gmail.com>
2025-06-12 09:00:25 -0500

    Add regional field to Sede interface for improved sede management

--- src/pages/configuracion/maestros/interfaces/sedes.ts
+++ src/pages/configuracion/maestros/interfaces/sedes.ts
@@ -3,6 +3,7 @@ import { Usuario } from "../../usuarios/interfaces/usuarios";
 export interface Sede {
   id_sede: number;
   nombre: string;
+  regional: string;
   usuarios: Usuario[];
   estado: boolean;
 }
