10a0ffe7f29d4752f8ab110827c73be96ce4833f
JesusPRios <juguitocifrut@gmail.com>
2025-06-15 12:05:23 -0500

    Creation of the styles to generate PDF of an acta

---
+++ src/pages/productos/views/stylesActa.tsx
@@ -0,0 +1,82 @@
+import { StyleSheet } from "@react-pdf/renderer";
+
+export const styles = StyleSheet.create({
+  page: {
+    flexDirection: "column",
+    padding: 30,
+    fontFamily: "Helvetica",
+  },
+  header: {
+    fontSize: 18,
+    marginBottom: 20,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  subheader: {
+    fontSize: 11,
+    marginBottom: 15,
+  },
+  table: {
+    display: "flex",
+    width: "auto",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderRightWidth: 0,
+    borderBottomWidth: 0,
+  },
+  tableRow: {
+    margin: "auto",
+    flexDirection: "row",
+  },
+  tableCol: {
+    width: "25%",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderLeftWidth: 0,
+    borderTopWidth: 0,
+  },
+  tableCell: {
+    margin: 5,
+    fontSize: 10,
+  },
+  sectionTitle: {
+    fontSize: 12,
+    marginTop: 15,
+    marginBottom: 5,
+    fontWeight: "bold",
+  },
+  text: {
+    fontSize: 10,
+    marginBottom: 10,
+  },
+  signatures: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 50,
+    paddingHorizontal: 20,
+  },
+
+  signature: {
+    width: "40%",
+    alignItems: "center",
+    fontSize: 10,
+  },
+
+  firmaImagen: {
+    width: 150,
+    height: 70,
+    marginBottom: 5,
+  },
+
+  signatureLine: {
+    borderTopWidth: 1,
+    borderColor: "#000",
+    width: "100%",
+    marginBottom: 5,
+  },
+
+  signatureRole: {
+    fontSize: 10,
+    marginTop: 2,
+  },
+});
