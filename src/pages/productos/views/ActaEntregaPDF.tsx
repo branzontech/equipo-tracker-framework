f0c57c56533195806cd56fd2f4438e9a7c346c98
JesusPRios <juguitocifrut@gmail.com>
2025-06-15 12:08:11 -0500

    Render PDF by act type (Prestamo, Traslado, Baja)

---
+++ src/pages/productos/views/ActaEntregaPDF.tsx
@@ -0,0 +1,47 @@
+import { Document, Page, Text } from "@react-pdf/renderer";
+import { ActaPrestamo } from "./ActaPrestamo";
+import { ActaTraslado } from "./ActaTraslado";
+import { ActaBaja } from "./ActaBaja";
+
+export const ActaEntregaPDF = ({ data }) => {
+  if (!data?.tipo) {
+    return (
+      <Document>
+        <Page>
+          <Text>Acta no válida</Text>
+        </Page>
+      </Document>
+    );
+  }
+  if (data.tipo === "Prestamo") {
+    return (
+      <Document>
+        <ActaPrestamo data={data} />
+      </Document>
+    );
+  }
+
+  if (data.tipo === "Traslado") {
+    return (
+      <Document>
+        <ActaTraslado data={data} />
+      </Document>
+    );
+  }
+
+  if (data.tipo === "Baja") {
+    return (
+      <Document>
+        <ActaBaja data={data} />
+      </Document>
+    );
+  }
+
+  return (
+    <Document>
+      <Page>
+        <Text>Tipo de acta no soportado: {data.tipo}</Text>
+      </Page>
+    </Document>
+  );
+};
