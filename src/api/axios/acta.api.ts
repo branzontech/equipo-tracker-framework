38360fc0c87808a692eb1206dbfb5fa0fd8085e6
JesusPRios <juguitocifrut@gmail.com>
2025-06-15 00:03:51 -0500

    Add updateStatus function to handle status updates for actas

--- src/api/axios/acta.api.ts
+++ src/api/axios/acta.api.ts
@@ -4,3 +4,12 @@ export const get = async () => {
   const { data } = await api.get("/actas/get");
   return data;
 };
+
+export const updateStatus = async (id, newStatus, tipo) => {
+  const { data } = await api.post("/actas/updateStatus", {
+    id,
+    newStatus,
+    tipo,
+  });
+  return data;
+};
