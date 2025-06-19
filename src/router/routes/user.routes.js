8cf4df8ed66f6ac4947c298afc2d3adb511b9ab8
JesusPRios <juguitocifrut@gmail.com>
2025-06-18 23:34:54 -0500

    Add route for retrieving user by name in user.routes

--- src/router/routes/user.routes.js
+++ src/router/routes/user.routes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { findAll } from "../../pages/usuarios/controller/user.controller.js";
+import {
+  findAll,
+  findByName,
+} from "../../pages/usuarios/controller/user.controller.js";
 const router = Router();
 
-router.get("/get", findAll);
+router
+    .get("/get", findAll)
+    .get("/get/:name", findByName);
 
 export default router;
