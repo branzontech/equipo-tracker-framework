63eae165a244303bd234d25c2ab2445d1fecfb87
JesusPRios <juguitocifrut@gmail.com>
2025-06-19 16:04:27 -0500

    Add usuarios_prestamos_responsable_salida_idTousuarios field to Prestamo interface

--- src/pages/productos/interfaces/prestamo.ts
+++ src/pages/productos/interfaces/prestamo.ts
@@ -6,12 +6,16 @@ export interface Prestamo {
   actas: Acta | null;
   responsable_salida_id: number;
   responsable_entrada_id: number;
+  usuarios_prestamos_responsable_salida_idTousuarios: {
+    id_usuario: number;
+    nombre: string;
+  };
   fecha_salida: Date | null;
   fecha_retorno: Date | null;
   estado: string;
   descripcion: string;
   equipos: {
     id_equipo: number;
-    perifericos: number[]; // IDs de los periféricos asociados a este equipo
+    perifericos: number[]; 
   }[];
 }
