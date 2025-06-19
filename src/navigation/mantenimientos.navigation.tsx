062406da5d4573ad7cf3fc8c693eb74d77e74e7c
JesusPRios <juguitocifrut@gmail.com>
2025-06-18 23:33:14 -0500

    Refactor mantenimientos navigation: improve formatting and add EditEquipo import

--- src/navigation/mantenimientos.navigation.tsx
+++ src/navigation/mantenimientos.navigation.tsx
@@ -4,12 +4,19 @@ import EjecucionMantenimiento from "@/pages/mantenimientos/Ejecucion";
 import DocumentacionMantenimiento from "@/pages/mantenimientos/Documentacion";
 import AuditoriaMantenimiento from "@/pages/mantenimientos/Auditoria";
 import Details from "@/pages/mantenimientos/Details";
+import EditEquipo from "@/pages/productos/views/EditEquipo";
 
 export const mantenimientosNavigation = [
   { path: "/mantenimientos", element: <MantenimientosIndex /> },
-  { path: "/mantenimientos/programacion", element: <ProgramacionMantenimiento /> },
+  {
+    path: "/mantenimientos/programacion",
+    element: <ProgramacionMantenimiento />,
+  },
   { path: "/mantenimientos/ejecucion", element: <EjecucionMantenimiento /> },
-  { path: "/mantenimientos/documentacion", element: <DocumentacionMantenimiento /> },
+  {
+    path: "/mantenimientos/documentacion",
+    element: <DocumentacionMantenimiento />,
+  },
   { path: "/mantenimientos/auditoria", element: <AuditoriaMantenimiento /> },
-  {path: "/mantenimientos/detalles/:id", element: <Details />},
+  { path: "/mantenimientos/detalles/:id", element: <Details /> },
 ];
