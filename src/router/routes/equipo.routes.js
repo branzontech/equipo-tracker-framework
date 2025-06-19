14663cb32b9c1b7139aec6a1d7c6f92222a6ad16
JesusPRios <juguitocifrut@gmail.com>
2025-06-18 23:34:51 -0500

    Add update route to equipo.routes for updating equipment records

--- src/router/routes/equipo.routes.js
+++ src/router/routes/equipo.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { create, findAll, findAllById, delete_ } from "../../pages/productos/controller/equipo.controller.js";
+import { create, findAll, findAllById, delete_, update } from "../../pages/productos/controller/equipo.controller.js";
 
 const router = express.Router();
 
 router
     .post("/create", create)
+    .post("/update", update)
     .get("/get", findAll)
     .get("/:nro_serie", findAllById)
     .delete("/:id", delete_);
