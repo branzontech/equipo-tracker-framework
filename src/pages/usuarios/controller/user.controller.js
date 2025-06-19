f7ee12c8d79060678b585c54fd1ae6bdce95db73
JesusPRios <juguitocifrut@gmail.com>
2025-06-18 23:34:41 -0500

    Add findByName function to user controller for retrieving user by name

--- src/pages/usuarios/controller/user.controller.js
+++ src/pages/usuarios/controller/user.controller.js
@@ -8,3 +8,13 @@ export const findAll = async (req, res) => {
     res.status(401).json({ error: error.message });
   }
 };
+
+export const findByName = async (req, res) => {
+  try {
+    const { name } = req.params;
+    const user = await UserService.findByName(name);
+    res.json({ usuario: user });
+  } catch (error) {
+    res.status(401).json({ error: error.message });
+  }
+};
