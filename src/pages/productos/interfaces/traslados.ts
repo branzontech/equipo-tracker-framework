de40a3cb69d4f74b453e6a250e28a9ac0dd65656
JesusPRios <juguitocifrut@gmail.com>
2025-06-19 16:04:36 -0500

    Add usuarios and sucursales fields to Traslado interface

--- src/pages/productos/interfaces/traslados.ts
+++ src/pages/productos/interfaces/traslados.ts
@@ -7,7 +7,23 @@ export interface Traslado {
   observaciones: string;
   responsable_salida_id: number;
   responsable_entrada_id: number;
+  usuarios: {
+    id_usuario: number;
+    nombre: string;
+  }
   sucursal_destino_id: number;
+  sucursales: {
+    id_sucursal: number;
+    nombre: string;
+    sedes: {
+      id_sede: number;
+      nombre: string;
+      usuarios: {
+        id_usuario: number;
+        nombre: string;
+      }[];
+    }[];
+  }
   equipos: {
     id_equipo: number;
     perifericos: number[]; 
