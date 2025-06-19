1a71c615c16ad51d931d5ad763de0fccc5686885
JesusPRios <juguitocifrut@gmail.com>
2025-06-18 23:32:54 -0500

    Add getUserByName function to retrieve user by name

--- src/api/axios/user.api.ts
+++ src/api/axios/user.api.ts
@@ -4,3 +4,8 @@ export const getUsers = async () => {
     const response = await api.get("/users/get");
     return response.data.usuarios;
 };
+
+export const getUserByName = async (name: string) => {
+    const response = await api.get(`/users/get/${name}`);
+    return response.data.usuario;
+};
