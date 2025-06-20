b3f1bcd644e4e23deda3c97bfc7d55012abaaa32
JesusPRios <juguitocifrut@gmail.com>
2025-06-19 22:10:44 -0500

    Format user.api.ts for consistent indentation and spacing

--- src/api/axios/user.api.ts
+++ src/api/axios/user.api.ts
@@ -1,11 +1,21 @@
+import { Usuario } from "@/pages/configuracion/usuarios/interfaces/usuarios";
 import api from "./axiosConfig";
 
 export const getUsers = async () => {
-    const response = await api.get("/users/get");
-    return response.data.usuarios;
+  const response = await api.get("/users/get");
+  return response.data.usuarios;
 };
 
 export const getUserByName = async (name: string) => {
-    const response = await api.get(`/users/get/${name}`);
-    return response.data.usuario;
+  const response = await api.get(`/users/get/${name}`);
+  return response.data.usuario;
+};
+
+export const createUser = async (usuario: Usuario) => {
+  try {
+    const response = await api.post("/users/create", usuario);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message);
+  }
 };
