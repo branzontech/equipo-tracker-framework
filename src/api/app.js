1dbae93fdeca108c91552af367ed4e09b19b0749
JesusPRios <juguitocifrut@gmail.com>
2025-06-20 10:54:09 -0500

    Refactor CORS configuration to use centralized corsOptions

--- src/api/app.js
+++ src/api/app.js
@@ -3,34 +3,11 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { config } from "../config/config.js";
 import routerApi from "../router/router.js";
+import { corsOptions } from "../middlewares/cors.js";
 
 dotenv.config();
 const app = express();
-
-// Configuración de CORS
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      const allowedOriginsPattern =
-        /^http:\/\/localhost:(8080|5173|5176|5174|3001)$/;
-      const allowedIpPattern =
-        /^http:\/\/192\.168\.1\.8:(8080|5173|5176|5174|3306|3002)$/;
-
-      if (
-        allowedOriginsPattern.test(origin) ||
-        allowedIpPattern.test(origin) ||
-        !origin
-      ) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
-
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 routerApi(app);
