377659f3d0cd88085666ff8228f71ca57726a6db
JesusPRios <juguitocifrut@gmail.com>
2025-06-19 22:11:58 -0500

    Add POST route for user creation in user.routes.js

--- src/router/routes/user.routes.js
+++ src/router/routes/user.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   findAll,
   findByName,
+  create,
 } from "../../pages/usuarios/controller/user.controller.js";
 const router = Router();
 
 router
     .get("/get", findAll)
-    .get("/get/:name", findByName);
+    .get("/get/:name", findByName)
+    .post("/create", create);
 
 export default router;
