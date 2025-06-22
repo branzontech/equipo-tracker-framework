3c384b26437d49e3f8222e4e569404d261efdfb8
JesusPRios <juguitocifrut@gmail.com>
2025-06-21 19:04:14 -0500

    Add proveedor API functions: Implement getAllProveedores and createProveedor

---
+++ src/api/axios/proveedor.api.ts
@@ -0,0 +1,16 @@
+import { Proveedor } from "@/pages/configuracion/maestros/interfaces/proveedor";
+import api from "./axiosConfig";
+
+export const getAllProveedores = async () => {
+  const response = await api.get("/proveedores/get");
+  return response.data;
+};
+
+export const createProveedor = async (data: Proveedor) => {
+  try {
+    const response = await api.post("/proveedores/create", data);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message);
+  }
+};
