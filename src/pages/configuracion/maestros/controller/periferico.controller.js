eb4b1430d94b2a4cb6cf387cec6e20e513a24d17
JesusPRios <juguitocifrut@gmail.com>
2025-06-23 15:02:44 -0500

    Refactor periferico.controller.js to include serial, id_sede, and marca_id in update and create methods for enhanced data handling

--- src/pages/configuracion/maestros/controller/periferico.controller.js
+++ src/pages/configuracion/maestros/controller/periferico.controller.js
@@ -13,26 +13,48 @@ export const findById = async (req, res) => {
 
 export const update = async (req, res) => {
   const { id } = req.params;
-  const { nombre, tipo, estado, equipo_asociado_id } = req.body;
+  const {
+    nombre,
+    tipo,
+    estado,
+    equipo_asociado_id,
+    serial,
+    id_sede,
+    marca_id,
+  } = req.body;
 
   const periferico = await perifericoService.update(id, {
     nombre,
     tipo,
     estado,
     equipo_asociado_id,
+    serial,
+    id_sede,
+    marca_id,
   });
 
   res.status(200).json({ success: true, data: periferico });
 };
 
 export const create = async (req, res) => {
-  const { nombre, tipo, estado, equipo_asociado_id } = req.body;
+  const {
+    nombre,
+    tipo,
+    estado,
+    equipo_asociado_id,
+    serial,
+    id_sede,
+    marca_id,
+  } = req.body;
 
   const periferico = await perifericoService.create({
     nombre,
     tipo,
     estado,
     equipo_asociado_id,
+    serial,
+    id_sede,
+    marca_id,
   });
 
   res.status(200).json({ success: true, data: periferico });
