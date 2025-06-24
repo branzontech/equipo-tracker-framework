53f3b16b2e5803dd1f6a3fe884fdd9ce1d3ba8e1
JesusPRios <juguitocifrut@gmail.com>
2025-06-24 18:23:32 -0500

    Add route for retrieving impresora information by serial number

--- src/router/routes/acta.routes.js
+++ src/router/routes/acta.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getActas, getInfoEquipo, updateStatus, getInfoPerifericos } from "../../pages/productos/controller/acta.controller.js";
+import { getActas, getInfoEquipo, updateStatus, getInfoPerifericos, getInfoImpresoras } from "../../pages/productos/controller/acta.controller.js";
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router
     .get("/get", getActas)
     .get("/getEquipo/:nro_serie", getInfoEquipo)
     .get("/getPeriferico/:serial", getInfoPerifericos)
+    .get("/getImpresora/:serial", getInfoImpresoras)
     .post("/updateStatus", updateStatus)
 
 
