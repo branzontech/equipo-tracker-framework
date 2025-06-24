e7e2975b1ed1b2367dd74e996a00787a07777cff
JesusPRios <juguitocifrut@gmail.com>
2025-06-23 23:34:42 -0500

    Add getInfoPeriferico function to actaService for retrieving peripheral information by serial number

--- src/pages/productos/services/acta.service.js
+++ src/pages/productos/services/acta.service.js
@@ -7,6 +7,9 @@ export const actaService = {
   async getInfoEquipo(nro_serie) {
     return await ActaModel.getInfoEquipo(nro_serie);
   },
+  async getInfoPeriferico(serial) {
+    return await ActaModel.getInfoPeriferico(serial);
+  },
   async update(id, newStatus, tipo, acta_equipos) {
     return await ActaModel.updateStatus(id, newStatus, tipo, acta_equipos);
   }
