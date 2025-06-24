d2114f93dea2fee0b4fe379e74439358adf16e38
JesusPRios <juguitocifrut@gmail.com>
2025-06-23 23:34:57 -0500

    Add route for retrieving peripheral information by serial number

--- src/router/routes/acta.routes.js
+++ src/router/routes/acta.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { getActas, getInfoEquipo, updateStatus } from "../../pages/productos/controller/acta.controller.js";
+import { getActas, getInfoEquipo, updateStatus, getInfoPerifericos } from "../../pages/productos/controller/acta.controller.js";
 
 const router = express.Router();
 
 router
     .get("/get", getActas)
     .get("/getEquipo/:nro_serie", getInfoEquipo)
+    .get("/getPeriferico/:serial", getInfoPerifericos)
     .post("/updateStatus", updateStatus)
 
 
