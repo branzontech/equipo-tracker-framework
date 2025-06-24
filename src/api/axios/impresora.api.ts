9cf77dfb98f36b955b17df60cf2a5d1b10c5e717
JesusPRios <juguitocifrut@gmail.com>
2025-06-24 18:20:04 -0500

    Add functions to retrieve and update printer information by ID and serial

--- src/api/axios/impresora.api.ts
+++ src/api/axios/impresora.api.ts
@@ -6,6 +6,26 @@ export const getImpresora = async () => {
   return response.data.impresoras;
 };
 
+export const getImpresoraById = async (id: number) => {
+  const response = await api.get(`/impresoras/get/${id}`);
+  return response.data;
+};
+
+export const getImpresoraBySerial = async (serial: string) => {
+  try {
+    const response = await api.get(`/impresoras/get/${serial}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching impresora by serial:", error);
+    throw error;
+  }
+};
+
+export const updateImpresora = async (id: number, impresora: Impresora) => {
+  const response = await api.put(`/impresoras/update/${id}`, impresora);
+  return response.data;
+};
+
 export const createImpresora = async (impresora: Impresora) => {
   const response = await api.post("/impresoras/create", impresora);
   return response;
