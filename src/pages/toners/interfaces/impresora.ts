f56628eb1c14a0dace705d62c1c0a97a96750d7b
JesusPRios <juguitocifrut@gmail.com>
2025-06-24 18:23:14 -0500

    Add serial and estado fields to Impresora interface

--- src/pages/toners/interfaces/impresora.ts
+++ src/pages/toners/interfaces/impresora.ts
@@ -1,9 +1,21 @@
+import { Marca } from "@/pages/configuracion/maestros/interfaces/marcas";
 import { Sucursal } from "@/pages/configuracion/maestros/interfaces/sucursales";
+import { Prestamo } from "@/pages/productos/interfaces/prestamo";
+import { Traslado } from "@/pages/productos/interfaces/traslados";
 
 export interface Impresora {
   id_impresora: number;
   nombre: string;
   modelo: string;
+  serial: string;
+  estado: string;
   sucursal_id: Sucursal | number;
   sucursales: Sucursal;
+  marcas: Marca;
+  tipo: string;
+
+  prestamos?: Prestamo;
+  traslado?: {
+    traslados: Traslado;
+  }[];
 }
