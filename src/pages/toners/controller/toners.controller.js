9d82247ab925548d2e152a9f701c59b9ce485280
JesusPRios <juguitocifrut@gmail.com>
2025-06-25 22:31:46 -0500

    Refactor response structure in getById, update, and delete methods for consistency

--- src/pages/toners/controller/toners.controller.js
+++ src/pages/toners/controller/toners.controller.js
@@ -13,7 +13,7 @@ export const getById = async (req, res) => {
   try {
     const { id } = req.params;
     const toner = await tonersService.getById(id);
-    res.status(200).json({ toner });
+    res.status(200).json(toner);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -31,10 +31,9 @@ export const createToner = async (req, res) => {
 
 export const updateToner = async (req, res) => {
   try {
-    const { id } = req.params;
     const toner = req.body;
-    const updatedToner = await tonersService.update(id, toner);
-    res.status(200).json({ updatedToner });
+    const updatedToner = await tonersService.update(toner);
+    res.status(200).json({ updatedToner, success: true });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -44,7 +43,7 @@ export const deleteToner = async (req, res) => {
   try {
     const { id } = req.params;
     const deletedToner = await tonersService.delete(id);
-    res.status(200).json({ deletedToner });
+    res.status(200).json({ deletedToner, success: true });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
