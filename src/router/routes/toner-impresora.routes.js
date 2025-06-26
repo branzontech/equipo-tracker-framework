e7aca47ba76dd93f5f065a59f856c4ba5d493b26
JesusPRios <juguitocifrut@gmail.com>
2025-06-25 22:32:45 -0500

    Add getById route to toner-impresora for retrieving toner by ID

--- src/router/routes/toner-impresora.routes.js
+++ src/router/routes/toner-impresora.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import {
   getAll,
+  getById,
 } from "../../pages/toners/controller/toner_impresora.controller.js";
 
 const router = express.Router();
 
 router
     .get("/get", getAll)
+    .get("/get/:id", getById);
 
 export default router;
