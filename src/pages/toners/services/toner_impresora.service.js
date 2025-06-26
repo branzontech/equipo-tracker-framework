d5f9ab6e6f696d0c6ab3464c4352dfeec3eb0064
JesusPRios <juguitocifrut@gmail.com>
2025-06-25 22:32:14 -0500

    Refactor TonerImpresoraService: add missing newline and correct method formatting

--- src/pages/toners/services/toner_impresora.service.js
+++ src/pages/toners/services/toner_impresora.service.js
@@ -3,7 +3,10 @@ import { tonerImpresoraModel } from "../../../db/models/toner_impresora.model.js
 export default class TonerImpresoraService {
   async getAll() {
     return tonerImpresoraModel.getAll();
-  }
+  };
+  async getById(id) {
+    return tonerImpresoraModel.getInfoByIdToner(id);
+  };
 }
 
 export const tonerImpresoraService = new TonerImpresoraService();
