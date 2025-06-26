bc8c9d830757a85bd50eb7ef6dfd44dd71195db3
JesusPRios <juguitocifrut@gmail.com>
2025-06-25 22:31:29 -0500

    Add ImpresoraForm route to toners navigation

--- src/navigation/toners.navigation.tsx
+++ src/navigation/toners.navigation.tsx
@@ -1,9 +1,12 @@
 import IngresoToner from "@/pages/toners/views/Ingreso";
 import ExistenciaToners from "@/pages/toners/views/Existencia";
 import SalidaToners from "@/pages/toners/views/Salida";
+import ImpresoraForm from "@/pages/toners/views/ImpresoraForm";
 
 export const tonersNavigation = [
   { path: "/toners/ingreso", element: <IngresoToner /> },
+  { path: "/impresoras/ingreso", element: <ImpresoraForm /> },
+  // { path: "/toners/edit-existencia", element: <ExistenciaToners /> },
   { path: "/toners/existencia", element: <ExistenciaToners /> },
   { path: "/toners/salida", element: <SalidaToners /> },
 ];
