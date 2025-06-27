7323ad94c0c26c7000338634805557b3d0588516
JesusPRios <juguitocifrut@gmail.com>
2025-06-26 19:37:48 -0500

    Add routes for getting toner by serial and creating salida toner

--- src/router/routes/toner.routes.js
+++ src/router/routes/toner.routes.js
@@ -5,6 +5,8 @@ import {
   createToner,
   updateToner,
   deleteToner,
+  getBySerial,
+  createSalidaToner
 } from "../../pages/toners/controller/toners.controller.js";
 
 const router = express.Router();
@@ -12,8 +14,10 @@ const router = express.Router();
 router
     .get("/get", getAll)
     .get("/get/:id", getById)
+    .get("/getToner/:serial", getBySerial)
     .post("/create", createToner)
     .put("/update/:id", updateToner)
     .delete("/delete/:id", deleteToner)
+    .post("/createSalidaToner", createSalidaToner);
 
 export default router;
