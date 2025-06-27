964e7a6ec2c7e9de23a2aa5e6178997ae2550402
JesusPRios <juguitocifrut@gmail.com>
2025-06-26 19:36:38 -0500

    Add createSalidaToner and getBySerial functions to toners API

--- src/api/axios/toners.api.ts
+++ src/api/axios/toners.api.ts
@@ -1,5 +1,6 @@
 import { Toner } from "@/pages/toners/interfaces/toners";
 import api from "./axiosConfig";
+import { SalidaToner } from "@/pages/toners/interfaces/salidaToner";
 
 export const getToners = async () => {
   const response = await api.get("/toners/get");
@@ -24,6 +25,24 @@ export const updateToner = async (id: number, toner: Toner) => {
 export const deleteToner = async (id: number) => {
   try {
     const response = await api.delete(`/toners/delete/${id}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message);
+  }
+};
+
+export const getBySerial = async (serial: string) => {
+  try {
+    const response = await api.get(`/toners/getToner/${serial}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response?.data?.message);
+  }
+};
+
+export const createSalidaToner = async (salidaToner: SalidaToner) => {
+  try {
+    const response = await api.post("/toners/createSalidaToner", salidaToner);
     return response.data;
   } catch (error) {
     throw new Error(error.response?.data?.message);
