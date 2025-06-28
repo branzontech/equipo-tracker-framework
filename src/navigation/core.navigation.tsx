455a1d8f423b330144dcf4c00341a9653b9a1461
JesusPRios <juguitocifrut@gmail.com>
2025-06-27 23:41:27 -0500

    Import Perfil in the coreNavigation

--- src/navigation/core.navigation.tsx
+++ src/navigation/core.navigation.tsx
@@ -1,11 +1,13 @@
 import Login from "@/pages/auth/views/Login";
 import Dashboard from "@/pages/dashboard/Index";
 import NotFound from "@/pages/NotFound";
+import { Perfil } from "@/pages/usuarios/Perfil";
 import { Navigate } from "react-router-dom";
 
 export const coreNavigation = [
   { path: "/", element: <Navigate to="/login" replace /> },
   { path: "/login", element: <Login /> },
   { path: "/dashboard", element: <Dashboard /> },
+  { path: "perfil", element: <Perfil /> },
   { path: "*", element: <NotFound /> },
 ];
