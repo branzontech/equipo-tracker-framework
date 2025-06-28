2facee8ed7c80c9a10bf1a0476d0955086f11353
JesusPRios <juguitocifrut@gmail.com>
2025-06-27 23:39:49 -0500

    add bcrypt password comparison for login

--- src/db/models/auth.model.js
+++ src/db/models/auth.model.js
@@ -1,13 +1,20 @@
 import { prisma } from "../../../prisma/prismaCliente.js";
+import bcrypt from "bcryptjs";
 
 export const AuthModel = {
   findByCredentials: async (email, contraseña) => {
     try {
       const user = await prisma.usuarios.findFirst({
-        where: { email, contrase_a: contraseña },
+        where: { email },
       });
 
       if (!user) {
+        throw new Error("Correo electronico o contraseña incorrectos");
+      }
+
+      const passwordValid = bcrypt.compare(contraseña, user.contrase_a);
+
+      if (!passwordValid) {
         throw new Error("Correo electronico o contraseña incorrectos");
       }
 
