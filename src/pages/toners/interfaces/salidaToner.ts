206a51b616112d5dc63c983ad60df1aa2c2359ad
JesusPRios <juguitocifrut@gmail.com>
2025-06-27 23:44:37 -0500

    Extend SalidaToner interface with related printer and user data

--- src/pages/toners/interfaces/salidaToner.ts
+++ src/pages/toners/interfaces/salidaToner.ts
@@ -1,3 +1,8 @@
+import { Sucursal } from "@/pages/configuracion/maestros/interfaces/sucursales";
+import { Toner } from "./toners";
+import { Impresora } from "./impresora";
+import { Usuario } from "@/pages/configuracion/usuarios/interfaces/usuarios";
+
 export interface SalidaToner {
   id_movimiento: number;
   toner_id: number;
@@ -5,6 +10,14 @@ export interface SalidaToner {
   fecha: Date;
   impresora_destino_id: number;
   usuario_id: number;
+  usuario_id_retira: number;
   sucursal_id: number;
   observaciones: string;
+  impresora_origen_id: number;
+  impresoras_salidatoners_impresora_origen_idToimpresoras: Impresora;
+  toner: Toner;
+  sucursales: Sucursal;
+  impresoras: Impresora;
+  usuarios_salidatoners_usuario_retiro_idTousuarios: Usuario;
+  usuarios: Usuario;
 }
