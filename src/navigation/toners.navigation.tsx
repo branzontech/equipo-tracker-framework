50686083cbf226939ea86a78f0e52dfdead2f3a6
JesusPRios <juguitocifrut@gmail.com>
2025-06-27 23:41:44 -0500

    Import ListadoSalidasToner component

--- src/navigation/toners.navigation.tsx
+++ src/navigation/toners.navigation.tsx
@@ -2,11 +2,12 @@ import IngresoToner from "@/pages/toners/views/Ingreso";
 import ExistenciaToners from "@/pages/toners/views/Existencia";
 import SalidaToners from "@/pages/toners/views/Salida";
 import ImpresoraForm from "@/pages/toners/views/ImpresoraForm";
+import { ListadoSalidasToner } from "@/pages/toners/views/ListadoSalidasToner";
 
 export const tonersNavigation = [
   { path: "/toners/ingreso", element: <IngresoToner /> },
   { path: "/impresoras/ingreso", element: <ImpresoraForm /> },
-  // { path: "/toners/edit-existencia", element: <ExistenciaToners /> },
   { path: "/toners/existencia", element: <ExistenciaToners /> },
   { path: "/toners/salida", element: <SalidaToners /> },
+  { path: "/toners/salidas/lista", element: <ListadoSalidasToner /> },
 ];
