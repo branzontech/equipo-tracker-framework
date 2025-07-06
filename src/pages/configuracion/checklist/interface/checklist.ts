ddedccd1bb05e350e2150d3d9fb071f16d782d51
JesusPRios <juguitocifrut@gmail.com>
2025-07-06 16:00:51 -0500

    Add more options in the ctipo_calificacion field

--- src/pages/configuracion/checklist/interface/checklist.ts
+++ src/pages/configuracion/checklist/interface/checklist.ts
@@ -2,7 +2,7 @@ export interface Checklist {
   id_plantilla: number;
   nombre: string;
   tipo_equipo: string;
-  tipo_calificacion: "ESTRELLAS" | "PORCENTAJE";
+  tipo_calificacion: "ESTRELLAS" | "PORCENTAJE" | "CATEGORIA" | "ESCALA";
   campos: string[];
   creado_por: string;
   usuarios: {
