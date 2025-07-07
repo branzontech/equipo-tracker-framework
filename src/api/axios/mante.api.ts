ca870b0089ba031b1a1ffd85fccd9df4d30dcfa0
JesusPRios <juguitocifrut@gmail.com>
2025-07-07 00:24:06 -0500

    Add checklist progress update and saveResponse API methods

--- src/api/axios/mante.api.ts
+++ src/api/axios/mante.api.ts
@@ -1,5 +1,6 @@
 import { Mantenimiento } from "@/pages/mantenimientos/interfaces/mantenimiento";
 import api from "./axiosConfig";
+import { ChecklistRespuestaData } from "@/pages/configuracion/checklist/interface/checklist";
 
 export const getAll = async () => {
   try {
@@ -59,6 +60,54 @@ export const getFiles = async () => {
 export const delete_ = async (id: string) => {
   try {
     const response = await api.delete(`/mantenimientos/delete/${id}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(error?.response?.data?.message);
+  }
+};
+
+export const actualizarProgreso = async (id: number, progreso: number) => {
+  try {
+    const response = await api.put(
+      `/mantenimientos/actualizar-progreso/${id}`,
+      {
+        progreso: progreso,
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(error?.response?.data?.message);
+  }
+};
+
+export const saveResponse = async (data: ChecklistRespuestaData) => {
+  try {
+    const response = await api.put(
+      `/mantenimientos/save-response/${data.mantenimientoId}`,
+      {
+        plantillaId: data.plantillaId,
+        tecnicoId: data.tecnicoId,
+        respuestas: data.respuestas,
+        calificacion: data.calificacion,
+        observaciones: data.observaciones,
+        fechaRealizacion: data.fechaRealizacion,
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error("Error al guardar el response:", error);
+    throw new Error(
+      error?.response?.data?.message || "Error al guardar el response"
+    );
+  }
+};
+
+export const getCheckListResponses = async (id: string) => {
+  try {
+    const response = await api.get(
+      `/mantenimientos/get-checklist-responses/${id}`
+    );
     return response.data;
   } catch (error) {
     throw new Error(error?.response?.data?.message);
