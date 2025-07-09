397f25364bf06c7b1ba54fad20b69ee513c99581
JesusPRios <juguitocifrut@gmail.com>
2025-07-08 19:20:00 -0500

    Add view and download actions for uploaded files

--- src/pages/mantenimientos/Documentacion.tsx
+++ src/pages/mantenimientos/Documentacion.tsx
@@ -1,19 +1,6 @@
-import { useEffect, useState } from "react";
-import {
-  ChevronLeft,
-  Search,
-  Upload,
-  FileText,
-  Paperclip,
-  X,
-  Eye,
-  Download,
-  PencilIcon,
-  XCircle,
-} from "lucide-react";
+import { ChevronLeft, FileText, Eye, Download, XCircle } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Table,
@@ -23,8 +10,6 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { Mantenimiento } from "./interfaces/mantenimiento";
-import { getFiles } from "@/api/axios/mante.api";
 import { useMantenimiento } from "./hooks/use-mantenimiento";
 import { formatFecha } from "@/hooks/use-global";
 
@@ -69,7 +54,7 @@ const DocumentacionMantenimiento = () => {
         </h1>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
+      {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
         <Card>
           <CardHeader>
             <CardTitle className="flex items-center gap-2 text-[#040d50]">
@@ -93,7 +78,7 @@ const DocumentacionMantenimiento = () => {
             </div>
           </CardContent>
         </Card>
-      </div>
+      </div> */}
 
       <Card>
         <CardContent className="p-0">
@@ -104,77 +89,109 @@ const DocumentacionMantenimiento = () => {
                   ID° Mantenimiento
                 </TableHead>
                 <TableHead className="text-[#040d50]">Equipo</TableHead>
+                <TableHead className="text-[#040d50]">Nombre</TableHead>
                 <TableHead className="text-[#040d50]">Tipo Doc.</TableHead>
                 <TableHead className="text-[#040d50]">Fecha</TableHead>
                 <TableHead className="text-[#040d50]">Subido por</TableHead>
-                <TableHead className="text-[#040d50]">Estado</TableHead>
                 <TableHead className="text-[#040d50]">Acciones</TableHead>
               </TableRow>
             </TableHeader>
             <TableBody>
-              {archivosConMantenimiento.map((archivo) => (
-                <TableRow
-                  key={archivo.id_archivo}
-                  className="border-b border-gray-200 hover:bg-slate-100"
-                >
-                  <TableCell className="text-[#040d50]">
-                    {`MANT${archivo.mantenimiento_id
-                      .toString()
-                      .padStart(3, "0")}`}
-                  </TableCell>
-                  <TableCell className="text-[#040d50]">
-                    {archivo.mantenimiento.equipos?.nombre_equipo}
-                  </TableCell>
-                  <TableCell className="text-[#040d50]">
-                    {archivo.tipo_archivo}
-                  </TableCell>
-                  <TableCell className="text-[#040d50]">
-                    {formatFecha(archivo.fecha_subida)}
-                  </TableCell>
-                  <TableCell className="text-[#040d50]">
-                    <span className="text-sm">
-                      {archivo.mantenimiento.usuarios?.nombre || "—"}
-                    </span>
-                  </TableCell>
-                  <TableCell className="text-[#040d50]">
-                    <span className="text-sm capitalize">
-                      {getEstadoBadge(archivo.mantenimiento.estado)}
-                    </span>
-                  </TableCell>
-                  <TableCell>
-                    <div className="flex gap-2">
-                      <Button
-                        variant="ghost"
-                        size="icon"
-                        className="slate-100"
-                        onClick={() => {
-                          openFileInNewTab({
-                            content: archivo.archivo.content,
-                            tipo: archivo.archivo.tipo,
-                            nombre: archivo.archivo.nombre,
-                          });
-                        }}
+              {archivosConMantenimiento.length > 0 ? (
+                archivosConMantenimiento.map((archivo) => (
+                  <TableRow
+                    key={archivo.id_archivo}
+                    className="border-b border-gray-200 hover:bg-slate-100"
+                  >
+                    <TableCell className="text-[#040d50]">
+                      {`MANT${archivo.mantenimiento_id
+                        .toString()
+                        .padStart(3, "0")}`}
+                    </TableCell>
+                    <TableCell className="text-[#040d50]">
+                      {archivo.mantenimiento.mantenimiento_detalle?.[0]?.equipos
+                        ?.nombre_equipo || "—"}
+                    </TableCell>
+                    <TableCell className="text-[#040d50]">
+                      {archivo.nombre_archivo || "—"}
+                    </TableCell>
+                    <TableCell className="text-[#040d50]">
+                      {archivo.tipo_archivo}
+                    </TableCell>
+                    <TableCell className="text-[#040d50]">
+                      {formatFecha(archivo.fecha_subida)}
+                    </TableCell>
+                    <TableCell className="text-[#040d50]">
+                      <span className="text-sm">
+                        {archivo.mantenimiento.usuarios?.nombre || "—"}
+                      </span>
+                    </TableCell>
+                    <TableCell>
+                      <div className="flex gap-2">
+                        <Button
+                          variant="ghost"
+                          size="icon"
+                          className="slate-100"
+                          onClick={() => {
+                            openFileInNewTab({
+                              content: archivo.archivo.content,
+                              tipo: archivo.archivo.tipo,
+                              nombre: archivo.archivo.nombre,
+                            });
+                          }}
+                        >
+                          <Eye className="h-5 w-5" />
+                        </Button>
+                        <Button
+                          variant="ghost"
+                          size="icon"
+                          className="slate-100"
+                          onClick={() => {
+                            downloadFile({
+                              content: archivo.archivo.content,
+                              tipo: archivo.archivo.tipo,
+                              nombre: archivo.archivo.nombre,
+                            });
+                          }}
+                        >
+                          <Download className="h-5 w-5" />
+                        </Button>
+                        <Button
+                          variant="ghost"
+                          size="icon"
+                          className="slate-100"
+                        >
+                          <XCircle className="h-5 w-5" />
+                        </Button>
+                      </div>
+                    </TableCell>
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell colSpan={7} className="py-10 text-center">
+                    <div className="flex flex-col items-center justify-center text-slate-500">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-10 w-10 mb-2 text-slate-400"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={1.5}
                       >
-                        <Eye className="h-5 w-5" />
-                      </Button>
-                      <Button variant="ghost" size="icon" className="slate-100"
-                        onClick={() => {
-                          downloadFile({
-                            content: archivo.archivo.content,
-                            tipo: archivo.archivo.tipo,
-                            nombre: archivo.archivo.nombre,
-                          });
-                        }}
-                      >
-                        <Download className="h-5 w-5" />
-                      </Button>
-                      <Button variant="ghost" size="icon" className="slate-100">
-                        <XCircle className="h-5 w-5" />
-                      </Button>
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M9.75 9.75h4.5M9.75 12.75h2.25M12 3.75C7.443 3.75 3.75 7.443 3.75 12s3.693 8.25 8.25 8.25 8.25-3.693 8.25-8.25S16.557 3.75 12 3.75z"
+                        />
+                      </svg>
+                      <span className="text-sm font-medium">
+                        No hay archivos disponibles en este momento.
+                      </span>
                     </div>
                   </TableCell>
                 </TableRow>
-              ))}
+              )}
             </TableBody>
           </Table>
         </CardContent>
