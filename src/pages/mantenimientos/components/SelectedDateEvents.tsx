ceba26a66977c6cbfabfb2f1b1d94b25ee1408e2
JesusPRios <juguitocifrut@gmail.com>
2025-07-09 12:10:09 -0500

    Replace format to formatFecha method

--- src/pages/mantenimientos/components/SelectedDateEvents.tsx
+++ src/pages/mantenimientos/components/SelectedDateEvents.tsx
@@ -1,7 +1,5 @@
-
 import React from "react";
-import { format } from "date-fns";
-import { es } from "date-fns/locale";
+import { formatFecha } from "@/hooks/use-global";
 import { Card, CardHeader, CardContent, CardTitle } from "@/components/ui/card";
 import { EventsList } from "./EventsList";
 import { SelectedDateEventsProps } from "@/pages/mantenimientos/components/interfaces/selectedDateEventsProps";
@@ -9,7 +7,7 @@ import { SelectedDateEventsProps } from "@/pages/mantenimientos/components/inter
 export const SelectedDateEvents: React.FC<SelectedDateEventsProps> = ({
   selectedDate,
   events,
-  estados
+  estados,
 }) => {
   if (!selectedDate) return null;
 
@@ -17,14 +15,14 @@ export const SelectedDateEvents: React.FC<SelectedDateEventsProps> = ({
     <Card>
       <CardHeader className="pb-2">
         <CardTitle className="text-lg">
-          Eventos del {format(selectedDate, "d 'de' MMMM", { locale: es })}
+          Eventos del {formatFecha(selectedDate)}
         </CardTitle>
       </CardHeader>
       <CardContent className="max-h-[400px] overflow-y-auto">
-        <EventsList 
-          events={events} 
-          estados={estados} 
-          emptyMessage="No hay eventos para esta fecha" 
+        <EventsList
+          events={events}
+          estados={estados}
+          emptyMessage="No hay eventos para esta fecha"
         />
       </CardContent>
     </Card>
